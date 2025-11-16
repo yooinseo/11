5a948dee1544ab96ec6a8d859b2bcc0fce1faf94
yooinseo <qetu37@sookmyung.ac.kr>
2025-11-16 21:42:14 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,20 +3,12 @@
 
 int main(int argc, char *argv[])
 {
-   int i=10;
-    char c='a';
-    int *iptr;
-    char *cptr;
-    int *iptr2;
+  int i=300;
 
-    iptr = &i;
-    cptr = &c;
-    iptr2 = iptr;
+    int *pi = &i;
+    char *pc = &i;
 
-    printf("i:%p\n%p(size:%i)\n", iptr, &i, sizeof(iptr));
-    printf("c:%p\n%p(size:%i)\n", cptr, &c, sizeof(cptr));
-    printf("iptr2:%p, %i\n\n", iptr2, *iptr2);
-
+    printf("%i, %i, %i\n", i, *pi, *pc);
   system("PAUSE");	
   return 0;
 }
