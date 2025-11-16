401225b4a69a0d85f1d691ef75fba72642b51f2d
yooinseo <qetu37@sookmyung.ac.kr>
2025-11-16 22:01:04 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+char *proverb = "All that glisters...";
+
+void setPointer(char **q)
+{
+    *q = proverb;
+}
 
 int main(int argc, char *argv[])
 {
-    int i = 100;
-    int *p = &i;
-    int **q = &p;
+    char *p = "zzz";
 
-    *p = 200;
-    printf("i = %i, *p = %d, **q = %d\n", i, *p, **q);
-
-    **q = 300;
-    printf("i = %i, *p = %d, **q = %d\n", i, *p, **q);
-
+    setPointer(&p);
+    printf("%s \n", p);
 
   system("PAUSE");	
   return 0;
