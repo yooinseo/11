895c37ec8adfa0d877c35f8c0e22951a943393a2
yooinseo <qetu37@sookmyung.ac.kr>
2025-11-16 21:58:31 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,23 +3,16 @@
 
 int main(int argc, char *argv[])
 {
-    int i;
-    int grade[5];
-    int sum = 0;
+    int i = 100;
+    int *p = &i;
+    int **q = &p;
 
-    for (i = 0; i < 5; i++)
-    {
-        printf("grade[%i] = ", i);
-        scanf("%d", &grade[i]);
-    }
+    *p = 200;
+    printf("i = %i, *p = %d, **q = %d\n", i, *p, **q);
 
-    for (i = 0; i < 5; i++)
-    {
-        printf("grade[%i] = %i\n", i, *(grade + i));
-        sum = sum + *(grade + i);
-    }
+    **q = 300;
+    printf("i = %i, *p = %d, **q = %d\n", i, *p, **q);
 
-    printf("Average : %i\n", sum / 5);
 
   system("PAUSE");	
   return 0;
