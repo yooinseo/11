1977201059a6a89d6728e07f4398ec8b7ccf2ffb
yooinseo <qetu37@sookmyung.ac.kr>
2025-11-16 21:48:48 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void swap(int *x, int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
 int main(int argc, char *argv[])
 {
-  int i=300;
+    int a = 3;
+    int b = 5;
 
-    int *pi = &i;
-    char *pc = &i;
+    swap(&a, &b);
+    printf("a:%i, b:%i\n", a, b);
 
-    printf("%i, %i, %i\n", i, *pi, *pc);
   system("PAUSE");	
   return 0;
 }
